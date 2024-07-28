fcfebf781ff33868df7bcc469afe6744af4e4148
Valentin Rymaszewski <rymaszewski65@gmail.com>
2024-07-28 20:34:17 -0300

    env mongo

--- src/app.js
+++ src/app.js
@@ -8,15 +8,15 @@ import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 
 const app = express();
-const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+const PORT = process.env.PORT || 8080;
+const connection = mongoose.connect(process.env.MONGO_URL, { dbName: process.env.DB_NAME });
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
