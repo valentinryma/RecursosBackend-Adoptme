4e907ebab22b51b2cab43a87ed9267dcae17aaf3
Valentin Rymaszewski <rymaszewski65@gmail.com>
2024-08-05 20:51:18 -0300

    add views

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import { engine } from 'express-handlebars';
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import viewsRouter from './routes/views.router.js';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -14,10 +16,15 @@ const connection = mongoose.connect(process.env.MONGO_URL, { dbName: process.env
 app.use(express.json());
 app.use(cookieParser());
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', 'src/views');
+
 app.use('/api/users', usersRouter);
 app.use('/api/pets', petsRouter);
 app.use('/api/adoptions', adoptionsRouter);
 app.use('/api/sessions', sessionsRouter);
+app.use('/', viewsRouter);
 
 app.use(
     '/saludo',
